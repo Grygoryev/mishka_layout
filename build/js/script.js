95c60133cc28fd60cf8423b56463a81a2938302f
Nickel 13 <nikesushko@gmail.com>
2017-12-28 14:37:44 +0300

    22nd

--- build/js/script.js
+++ build/js/script.js
@@ -5,21 +5,14 @@ var focus = popup.querySelector("[name=name]");
 
 open.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("items-btn--off");
-  popup.classList.add("mobile-menu-show");
-  focus.focus();
+  open.classList.toggle("element-switch--off");
+  popup.classList.toggle("element-switch--off");
+  close.classList.toggle("element-switch--off");
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.remove("mobile-menu-show");
-});
-
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("modal-window-show")) {
-      popup.classList.remove("modal-window-show");
-      popup.classList.add("mobile-menu__items-btn");
-    }
-  }
+  open.classList.toggle("element-switch--off");
+  popup.classList.toggle("element-switch--off");
+  close.classList.toggle("element-switch--off");
 });
